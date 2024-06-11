e4e49f0f47d691da30b31b29eeca70cf95ec52d6
Automated MCCS User <rdiusr@hqm04ibisvr0010.usmc-mccs.org>
2024-06-11 15:03:13 -0400

    Added high jump and new style barcode dups

---
+++ high_jump/hj_unprocessed_invent_snap_warning.pl
@@ -0,0 +1,112 @@
+#!/usr/local/mccs/perl/bin/perl
+##--------------------------------------------------------------------------
+##Ported by : Soumya K Bhowmic
+##Date      : 12/13/2023
+##
+##Brief Desc: This program generates a report for High Jump (HJ) for any 
+##            inventory snap short host_group_id that has not been processed 
+##            yet in T_AL_HOST_SQL_EXPORT_QUEUE_ARC table. 
+##            Need to delete or update the host group id after examing the case.
+##
+##            The program must run a rdiusr.
+## --------------------------------------------------------------------------  
+use strict;
+use IBIS::DBI;
+use Data::Dumper;
+use IBIS::EDI;
+use IBIS::E856;
+use IBIS::EWMS;
+use Net::SFTP::Foreign;
+use POSIX qw(strftime WNOHANG);
+use IBIS::Log::File;
+use IBIS::DBI;
+use IBIS::Email;
+use Fcntl qw(:DEFAULT :flock);
+my ($debug);
+
+## Options
+
+## Only one process running at a time.
+open( SELF, "<", $0 ) or die "Cannot open $0 - $!";
+flock( SELF, LOCK_EX | LOCK_NB ) or die "Already running.";
+
+## config file, object, db connections
+my $config = "/usr/local/mccs/etc/edi_wms/edi_wms.conf";
+my $wms = IBIS::EWMS->new( conf_file => $config );
+
+## variables
+my $now               = strftime( '%Y-%m-%d %H:%M:%S', localtime );
+my $d_suffix          = strftime( '%Y%m%d', localtime );
+my $t_suffix          = strftime( '%Y%m%d_%H%M%S', localtime );
+my $log_filename      = $wms->{LOG_DIR} . "/" . "hj_unprocessed_invent_snap_warning" 
+                        . $t_suffix;
+   $debug             = $wms->{DEBUG_WMS_PO};
+my $hostname          = `hostname`;
+## log obj
+$wms->{'log_obj'}     = IBIS::Log::File->new(
+    {
+        file   => $log_filename,
+        append => 1
+    }
+);
+$wms->{'log_obj'}->log_info("\n\n*************\n\tStarted at:$now\n");
+$wms->sget_dbh_obj();
+$wms->{'log_obj'}->info("Start program: $0");
+if ($debug) {
+    $wms->{'debug'} = 1;
+    my $obj_buffer = Dumper($wms);
+    print $obj_buffer;
+    $wms->{'log_obj'}->info("$obj_buffer");
+}
+
+my $str_host_date = "Time: ".$t_suffix  . "  Server:" . $hostname; 
+
+my $query = qq(
+     select host_group_id
+      from T_AL_HOST_SQL_EXPORT_QUEUE_ARC 
+      where export_type ='INVENTORY' 
+         AND STATUS ='P' 
+);
+
+my $value_ref;
+eval{
+    $value_ref = $wms->{'dbh_obj'}->selectall_arrayref($query);   
+};
+if ($@){	
+    my $msg = "DB selection query errored: $@";
+    $wms->{'log_obj'}->info($msg);
+}
+
+my $subject = 'UNPROCESSED HJ INVENTORY SNAP SHOT!';
+my $contents ='';
+my $email_address ='rdistaff@usmc-mccs.org';
+my $from = 'rdistaff@usmc-mccs.org';
+
+for ( my $i = 0 ; $i < @$value_ref ; $i++ ) {
+    $contents .= "\n select * from T_AL_HOST_SQL_EXPORT_QUEUE_ARC where host_group_id ="
+	."\'"."$value_ref->[$i]->[0]" ."\'";
+    $contents .= "\n update T_AL_HOST_SQL_EXPORT_QUEUE_ARC set status ='C' where host_group_id ="
+	."\'"."$value_ref->[$i]->[0]" ."\'";
+ }
+$contents  .= "\n\n";
+$contents .= $str_host_date; 
+
+if(@$value_ref > 0){
+    $wms->{'log_obj'}->info($contents);
+    &sendmail( $email_address, $from, $subject, $contents ); 
+}else{
+    $wms->{'log_obj'}->info("No unprocessed host group id for today.");
+}
+
+## log end time
+my $end      = strftime( '%Y-%m-%d %H:%M:%S', localtime );
+$wms->{'log_obj'}->info("Ending time: $end\n");
+$wms->destructor();
+
+=head
+This is a quick report for any hj inventory snap short host_group_id that has not been processed yet in 
+ T_AL_HOST_SQL_EXPORT_QUEUE_ARC table. Need to delete or update the host group id after examing the case. 
+
+=cut
+
+##################
---
+++ high_jump/hj_wms_invent_snap.pl
@@ -0,0 +1,120 @@
+#!/usr/local/mccs/perl/bin/perl
+##--------------------------------------------------------------------------
+##Ported by : Soumya K Bhowmic
+##Date      : 12/08/2023
+##
+##Brief Desc: This program extracts the UPC codes and the associated
+##            description for FraudWatch (TE-Triversity)
+##
+##            The program must run a rdiusr.
+## --------------------------------------------------------------------------  
+use strict;
+use IBIS::DBI;
+use Data::Dumper;
+use IBIS::EDI;
+use IBIS::E856;
+use IBIS::EWMS;
+use Net::SFTP::Foreign;
+use POSIX qw(strftime WNOHANG);
+use IBIS::Log::File;
+use IBIS::DBI;
+
+use Fcntl qw(:DEFAULT :flock);
+my ($debug, $ftp_result, @errors);
+## one process at a time.
+open( SELF, "<", $0 ) or die "Cannot open $0 - $!";
+flock( SELF, LOCK_EX | LOCK_NB ) or die "Already running.";
+
+my $disable_dir = '/usr/local/mccs/data/wms/data/disable';
+my $disable_f = $disable_dir . '/' . $0;
+if (-e $disable_f) { exit 0; }
+
+## Options
+my  $config = "/usr/local/mccs/etc/edi_wms/edi_wms.conf";
+## Object
+my $wms = IBIS::EWMS->new( conf_file => $config );
+
+## config file, object, db connections
+##  Log start time
+my $now               = strftime( '%Y-%m-%d %H:%M:%S', localtime );
+my $d_suffix          = strftime( '%Y%m%d', localtime );
+my $t_suffix          = strftime( '%Y%m%d_%H%M%S', localtime );
+my $log_filename      = $wms->{LOG_DIR} . "/" . "hj_invent_log_" . $t_suffix;
+
+   $debug             = $wms->{DEBUG_WMS_INV};
+my $site_list;
+
+## log obj
+$wms->{'log_obj'}     = IBIS::Log::File->new(
+    {
+        file   => $log_filename,
+        append => 1
+    }
+);
+$wms->{'log_obj'}->log_info("\n\n*************\n\tStarted at:$now\n");
+$wms->sget_dbh_obj();
+$wms->{'log_obj'}->info("Start program: $0");
+if ($debug) {
+    $wms->{'debug'} = 1;
+    my $obj_buffer = Dumper($wms);
+    print $obj_buffer;
+    $wms->{'log_obj'}->info("$obj_buffer");
+}
+
+
+&run_invent_stream;
+sleep(2);
+&run_report_on_variance_tab;
+
+sleep(30);
+
+&run_invent_stream;
+sleep(2);
+&run_report_on_variance_tab;
+
+## all the log info, and error messages email out...
+my $end      = strftime( '%Y-%m-%d %H:%M:%S', localtime );
+$wms->{'log_obj'}->info("Ending time: $end\n");
+$wms->destructor();
+
+
+## exec rdiusr.MRI_HJ_IMPORT_INVENT_SNP.STREAM_LINE_INVENTORY_SNAPSHOT;
+sub run_invent_stream {
+    eval{
+	my $sth1 = $wms->{'dbh_obj'}->prepare('BEGIN rdiusr.MRI_HJ_IMPORT_INVENT_SNP.STREAM_LINE_INVENTORY_SNAPSHOT; END;');
+	$sth1->execute || die $wms->{'log_obj'}->info($wms->{'dbh_obj'}->errstr);
+    };
+    if($@){
+	my $msg = "Database errors when exec claim db procedures:.  rdiusr.MRI_HJ_IMPORT_INVENT_SNP.STREAM_LINE_INVENTORY_SNAPSHOT... aborted, aborted...";
+	$wms->{'log_obj'}->info($msg);
+	die;
+    }
+}
+
+
+sub run_report_on_variance_tab {
+
+    my $query = "select (sysdate - (select max (date_processed) from  HJ_WMS_TOT_HJI_RMS_VARIANCE) ) as date_diff from dual";
+
+    my $ret_val = $wms->{'dbh_obj'}->selectall_arrayref($query);
+
+    if ( $ret_val->[0][0] < 0.15 ) { ## if the last job is less than 15 mins old, do report.
+	
+	my $cmd = '/usr/local/mccs/perl/bin/perl /usr/local/mccs/bin/high_jump/hj_rms_daily_inv_var_rep';
+	
+	eval{
+	    system($cmd);
+	};
+
+	if($@){
+	    my $msg = "Error happened in running: /usr/local/mccs/perl/bin/perl /usr/local/mccs/bin/high_jump/hj_rms_daily_inv_var_rep";
+	    $wms->{'log_obj'}->info($msg);
+	    die;
+	}
+    }
+}
+
+
+=head
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+=cut
