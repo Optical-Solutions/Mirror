c20918bc413908572d53403d2a80f7d3176a8334
Kaveh Sari <ksari6000@yahoo.com>
2024-06-12 12:25:37 -0400

    Added kaveh sari for mail and changed main query to ignore status p and include only 10 rows.

--- high_jump/hj_unprocessed_invent_snap_warning.pl
+++ high_jump/hj_unprocessed_invent_snap_warning.pl
@@ -65,7 +65,10 @@ my $query = qq(
      select host_group_id
       from T_AL_HOST_SQL_EXPORT_QUEUE_ARC 
       where export_type ='INVENTORY' 
-         AND STATUS ='P' 
+      -- #TODO remove next line
+      and ronum < 10
+    -- #TODO uncomment next line.
+ --       AND STATUS ='P' 
 );
 
 my $value_ref;
@@ -79,7 +82,9 @@ if ($@){
 
 my $subject = 'UNPROCESSED HJ INVENTORY SNAP SHOT!';
 my $contents ='';
-my $email_address ='rdistaff@usmc-mccs.org';
+#TODO unocmment next line and delete line after that
+#my $email_address ='rdistaff@usmc-mccs.org';
+my $email_address ='kaveh.sari@usmc-mccs.org';
 my $from = 'rdistaff@usmc-mccs.org';
 
 for ( my $i = 0 ; $i < @$value_ref ; $i++ ) {
