ad54388d9158f039e546963fe0061344c6ccab80
Kaveh Sari <ksari6000@yahoo.com>
2024-07-03 14:01:59 -0400

    test

--- RPOS/test03.pl
+++ RPOS/test03.pl
@@ -1,7 +1,10 @@
   my $data_dir = '/usr/local/mccs/data/rpos/archive/iri_pos_daily/';
   my $date_buffer = `ls -lar $data_dir`;
+  print $date_buffer . '\n';
   my @ary = split(/\n/, $date_buffer);
+  print @ary;
     my $size = @ary;
+  print $size;
     if($size < 10){
         print 'not enough files';
     }
