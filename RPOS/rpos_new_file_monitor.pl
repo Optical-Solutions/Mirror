a2e8ec6be8c0a8293ce5a19a359bfe7c89b71dd9
Kaveh Sari <ksari6000@yahoo.com>
2024-07-05 13:47:28 -0400

    Chaneged email routine

--- RPOS/rpos_new_file_monitor.pl
+++ RPOS/rpos_new_file_monitor.pl
@@ -70,7 +70,9 @@ if($red_flag1 && $red_flag2 && $red_flag3){
 
 ###### subroutine ###################
 sub email_notification{
-    my $cc_str = 'yuc@usmc-mccs.org|Ashley.Robey@usmc-mccs.org|gearhartj@usmc-mccs.org|hq.help@usmc-mccs.org|Daniel.Duenas@usmc-mccs.org|billy.vanover@usmc-mccs.org|larry.d.lewis@usmc-mccs.org|Nora.Jansen@usmc-mccs.org|';
+    #TODO uncomment next line, delete line after.
+    #my $cc_str = 'yuc@usmc-mccs.org|Ashley.Robey@usmc-mccs.org|gearhartj@usmc-mccs.org|hq.help@usmc-mccs.org|Daniel.Duenas@usmc-mccs.org|billy.vanover@usmc-mccs.org|larry.d.lewis@usmc-mccs.org|Nora.Jansen@usmc-mccs.org|';
+    my $cc_str = 'kaveh.sari@usmc-mccs.org';   
     ##my $cc_str_new  = 'yuc@usmc-mccs.org|MRI-RDIStaff@usmc-mccs.org|RPOSCoreTeam@usmc-mccs.org|esg@usmc-mccs.org|hq.help@usmc-mccs.org|';
     ## my $cc_str  = 'yuc@usmc-mccs.org|';
     my $content = 'If you are receiving this email, please contact the On-Call RPOS/MSST persons ASAP. Either no RPOS loading process was run for over 3 hours from an overwhelmed RMS DB job scheduler, Or TAS/upstream processes may have issues leading to no new sale files created in the past 3 hours. ';
