9709f7a8eba1509ef7801f82d5d7817f715bf394
Kaveh Sari <ksari6000@yahoo.com>
2024-07-11 10:29:30 -0400

    testing

--- pm/TMS.pm
+++ pm/TMS.pm
@@ -431,7 +431,7 @@ sub sftp_list_and_get {
     	    $self->{'log_obj'}->log_info("line 430\n");
 ## with name or without a name, anyways take the front space in names away:
     $local_file =~ s/\s+//g;
-    
+    $self->{'log_obj'}->log_info("right before sftp\n");
     my $ftp_result = $sftp->get( $remote_file, $local_file );
     if ($ftp_result) {
         $self->{'log_obj'}->log_info("\nSFTP successful!\n");
