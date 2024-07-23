1c63c4076caea3dc53f971ea2a75115d4feefcc0
Kaveh Sari <ksari6000@yahoo.com>
2024-07-23 16:26:29 -0400

    testing

--- pm/testsftp.pl
+++ pm/testsftp.pl
@@ -2,6 +2,19 @@ use Net::SFTP::Foreign;
 
 my $sftp = Net::SFTP::Foreign->new(
 	'landair-mft.mercurygate.net',
-	user     =>'landair',
+	user     => 'landair',
 	password => 'pG7n!mR9d')
 	|| die " Can not connect to remote server: landair-mft.mercurygate.net \n";
+print $sftp;
+
+# ==========================================================================================
+#  # Create an SFTP object
+#  my $sftp = Net::SFTP::Foreign->new($hostname, user => $username, password => $password);
+
+
+#  # Check for errors in creating the SFTP object
+#  die "Could not establish SFTP connection: " . $sftp->error unless defined $sftp;
+
+#  # Upload the local file
+#  $sftp->put($local_path, $remote_path) or die "Failed to upload file: " . $sftp->error;
+#  ========================================================================================
