0f30b6d63f5e20c503860e9441cb8328c0327808
Kaveh Sari <ksari6000@yahoo.com>
2024-07-23 16:19:43 -0400

    Added test file

---
+++ pm/testsftp.pl
@@ -0,0 +1,7 @@
+use Net::SFTP::Foreign;
+
+my $sftp = Net::SFTP->new(
+	'landair-mft.mercurygate.net',
+	user     =>'landair',
+	password => 'pG7n!mR9d')
+	) || die " Can not connect to remote server: landair-mft.mercurygate.net \n";
