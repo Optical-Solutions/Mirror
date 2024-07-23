32bfe0c0d6a80a6af9e8448c8633a42500bfca4d
Kaveh Sari <ksari6000@yahoo.com>
2024-07-23 16:20:40 -0400

    testing

--- pm/testsftp.pl
+++ pm/testsftp.pl
@@ -4,4 +4,4 @@ my $sftp = Net::SFTP->new(
 	'landair-mft.mercurygate.net',
 	user     =>'landair',
 	password => 'pG7n!mR9d')
-	) || die " Can not connect to remote server: landair-mft.mercurygate.net \n";
+	|| die " Can not connect to remote server: landair-mft.mercurygate.net \n";
