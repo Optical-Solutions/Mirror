02de5fdb898b2e386faf592c2ea777a209c3e95b
Kaveh Sari <ksari6000@yahoo.com>
2024-10-08 15:53:17 -0400

    Restored to nomral email (took email to kaveh sari out).

--- 856/edi_856_error_reporting.pl
+++ 856/edi_856_error_reporting.pl
@@ -77,11 +77,7 @@ unless($ret){
             my $th_str ='Asn_name|Vendor_id|PO|Error_msg|Log_name|';
             my $subject =  '856 Invalid Vendors (part1)';
             my $content = $e856->html_table_via_sth_or_aryref($ary_ref,$th_str,$subject,35);
-            #TODO remove fixed email, and uncomment next line , restore mail_cc line.
-            $e856->send_html_by_email($subject, 
-            'kaveh.sari@usmc-mccs.org',
-            #$e856->{MAIL_CC}, 
-            $content);
+            $e856->send_html_by_email($subject,$e856->{MAIL_CC},$content);
             ##$e856->send_report();
         }
         print Dumper($e856) if($e856->is_debug);
