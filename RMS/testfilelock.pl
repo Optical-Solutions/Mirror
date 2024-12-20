dfc7244075ddd646cc0f8bc7df27059039133edb
Kaveh Sari <ksari6000@yahoo.com>
2024-12-20 15:10:45 -0500

    test

--- RMS/testfilelock.pl
+++ RMS/testfilelock.pl
@@ -1,3 +1,4 @@
+use Fcntl qw(:flock);
 my $lock_file = "/tmp/lock2" . ".lck";
 open(my $fh, ">", $lock_file) or die "Could not create lock file $lock_file";
 flock $fh, LOCK_EX | LOCK_NB or die "Another $0 process already running";
