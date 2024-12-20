14b561df2ff98681cadb079355f12becc5576289
Kaveh Sari <ksari6000@yahoo.com>
2024-12-20 15:14:38 -0500

    test

--- RMS/testfilelock.pl
+++ RMS/testfilelock.pl
@@ -1,6 +1,6 @@
 use Fcntl qw(:flock);
-my $lock_file = "/tmp/lock2" . ".lck";
+my $lock_file = "/tmp/lock" . ".lck";
 open(my $fh, ">", $lock_file) or die "Could not create lock file $lock_file";
 flock $fh, LOCK_EX | LOCK_NB or die "Another $0 process already running";
+close $fh;  #TODO Needs verification.
 sleep 60;
-#close $fh;  #TODO Needs verification.
